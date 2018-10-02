751f52da3ce35d0f9de7e17dedc970abc135de1c
Vergess <h19951215p@163.com>
2018-10-02 20:58:27 +0800

    20181002

--- pythoncode/5.py
+++ pythoncode/5.py
@@ -22,8 +22,30 @@
 #     i=i+1
 # print('exce:%d,good:%d,normal:%d,pas:%d,bad:%d'%(a,b,c,d,e))
 # 3
-dic={}
-i=0
-while i<10:
-    print(i)
-    i=i+1 
+# dic={}
+# i=0
+# while i<10:
+#     key=input('please input the name:')
+#     value=eval(input('please input the score'))
+#     dic[key]=value
+#     i+=1
+# dic=sorted(dic.items(),key=lambda x:x[1],reverse=True)
+# print(dic)
+# 4
+# dic={'a':45,'b':78,'c':40,'d':96,'e':65,'f':90,'g':78,'h':99,'i':60,'j':87}
+# l=len(dic)
+# a=[];b=[]
+# for key,value in dic.items():
+#     a.append(key)
+#     b.append(value)
+# print(a)
+# print(b)
+# 5
+s=input('please input string')
+while len(s)!=0:
+    star=s[0]
+    num=s.count(star)
+    print('%s -> %d'%(star,num))
+    s=s.replace(star,' ')
+    s=s.strip()
+    
