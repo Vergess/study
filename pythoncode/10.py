22a5015999d85cd3610fb9561659e8664279f766
Vergess <h19951215p@163.com>
2019-01-08 09:56:31 +0800

    20190108

--- pythoncode/10.py
+++ pythoncode/10.py
@@ -1,1 +1,7 @@
 #1
+import wx
+class Frame(wx.Frame):
+    def __init__(self,superion):
+        wx.Frame.__init__(self,parent=superion,title='sum',size=(400,200))
+        panel=wx.Panel(self)
+        wx.StaticText(panel,label='input n:')
